0b452970bf0eefaf9357957f93636a2af6420204
JuanPablo Alba <juanpablo.alba@kellogg.northwestern.edu>
2021-04-13 11:58:55 -0500

    created code

--- noober.js
+++ noober.js
@@ -12,6 +12,39 @@ document.addEventListener('DOMContentLoaded', async function() {
 
   // 🔥 YOUR CODE GOES HERE 🔥
   // Write the recipe (algorithm) in the comments. Then, write the code.
+passengerFirstName=ride.passengerDetails.first
+passengerLastName=ride.passengerDetails.last
+passengerPhoneNumber=ride.passengerDetails.phoneNumber
+
+pickupAddress=ride.pickupLocation.address
+pickupCity=ride.pickupLocation.city 
+pickupState=ride.pickupLocation.state
+pickupZip=ride.pickupLocation.zip
+
+dropoffAddress=ride.dropoffLocation.address
+dropoffCity=ride.dropoffLocation.city 
+dropoffState=ride.dropoffLocation.state
+dropoffZip=ride.dropoffLocation.zip
+
+passengers=ride.numberOfPassengers
+
+let serviceLevel
+if (ride.purpleRequested){
+  serviceLevel=`Noober Purple`
+}
+else if (passengers>3){
+  serviceLevel=`Noober XL`
+}
+else{
+  serviceLevel=`Noober X`
+}
+  console.log(`${serviceLevel} Passenger: ${passengerFirstName} ${passengerLastName} 
+  ${passengerPhoneNumber}.
+  Pickup at: ${pickupAddress}, ${pickupCity}, ${pickupState}, ${pickupZip}.
+  Drop off at: ${dropoffAddress}, ${dropoffCity}, ${dropoffState} ${dropoffZip}.
+  Number of passengers: ${passengers}`)
   
+
+
   // 🔥 YOUR CODE ENDS HERE 🔥
 })
